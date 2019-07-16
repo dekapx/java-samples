fbeb50b964fb957261ef121adcfbde7c7a32f15f
KAPIL <kapil@KAPILs-MacBook-Pro.local>
2019-07-16 21:07:51 +0100

    immutable list example

---
+++ java-core/src/main/java/com/dekapx/java/collections/ImmutableListDemo.java
@@ -0,0 +1,28 @@
+package com.dekapx.java.collections;
+
+import com.google.common.collect.ImmutableList;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ImmutableListDemo {
+    public static void main(String[] args) {
+        immutableUsingCollections();
+        immutableUsingGuava();
+    }
+
+    private static void immutableUsingCollections() {
+        final List<String> numbers = Collections.unmodifiableList(Arrays.asList("One", "Two", "Three"));
+        print(numbers);
+    }
+
+    private static void immutableUsingGuava() {
+        final List<String> numbers = ImmutableList.copyOf(Arrays.asList("One", "Two", "Three"));
+        print(numbers);
+    }
+
+    private static void print(final List<String> numbers) {
+        numbers.forEach(System.out::println);
+    }
+}
--- java-core/src/main/java/com/dekapx/java/quickstart/Test.java
+++ java-core/src/main/java/com/dekapx/java/quickstart/Test.java
@@ -2,6 +2,6 @@ package com.dekapx.java.quickstart;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello Java");
     }
+
 }
