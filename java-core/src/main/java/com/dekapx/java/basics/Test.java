1d5a17210b8712e86e169c5a55af8bbdf8a0d6a3
KAPIL <kapil@KAPILs-MacBook-Pro.local>
2019-12-20 13:01:30 +0000

    biconsumer example

--- java-core/src/main/java/com/dekapx/java/basics/Test.java
+++ java-core/src/main/java/com/dekapx/java/basics/Test.java
@@ -1,10 +1,20 @@
 package com.dekapx.java.basics;
 
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiConsumer;
 
 public class Test {
     public static void main(String[] args) {
-        final String str = null;
-        System.out.println(Objects.isNull(str));
+        final List<String> languages = new ArrayList<>();
+        final String[] tokens = {"Java", "Scala", "Python"};
+        Arrays.stream(tokens).forEach(token -> biConsumer.accept(languages, token));
+
+        languages.forEach(System.out::println);
     }
+
+    final static BiConsumer<List<String>, String> biConsumer = (languages, token) -> {
+        languages.add(token);
+    };
 }
