c2207ce3da14f23c0eaef2a2ff9d8f2f9f895ec6
KAPIL <kapil@KAPILs-MacBook-Pro.local>
2020-01-28 22:49:29 +0000

    find the misisng positive number

--- java-core/src/main/java/com/dekapx/java/basics/Test.java
+++ java-core/src/main/java/com/dekapx/java/basics/Test.java
@@ -1,20 +1,53 @@
 package com.dekapx.java.basics;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.function.BiConsumer;
+import java.util.stream.IntStream;
 
+/**
+ that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+ For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+ Given A = [1, 2, 3], the function should return 4.
+ Given A = [−1, −3], the function should return 1.
+ */
 public class Test {
     public static void main(String[] args) {
-        final List<String> languages = new ArrayList<>();
-        final String[] tokens = {"Java", "Scala", "Python"};
-        Arrays.stream(tokens).forEach(token -> biConsumer.accept(languages, token));
+        int[] input4 = {1, 3, 6, 4, 5, 2, 8};
+        int[] input1 = {1, 3, 6, 4, 1, 2};
+        int[] input2 = {1, 2, 3};
+        int[] input3 = {-1, -3};
 
-        languages.forEach(System.out::println);
+        Test test = new Test();
+        int missing = test.solution(input1);
+        System.out.println(missing);
     }
 
-    final static BiConsumer<List<String>, String> biConsumer = (languages, token) -> {
-        languages.add(token);
-    };
+    public int solution(int[] input) {
+        int[] elements = IntStream.of(input).sorted().toArray();
+
+        int missing = -1;
+        for (int i = 0; i < elements.length; i ++) {
+            if (i < elements.length - 1) {
+                if (elements[i +1] - elements[i] > 1) {
+                    if (isPositive(elements[i])) {
+                        missing = elements[i] + 1;
+                    }
+                }
+            }
+        }
+
+        if (missing == -1) {
+            missing = elements[elements.length - 1] + 1;
+            while (true) {
+                if (!isPositive(missing)) {
+                    missing ++;
+                } else {
+                    break;
+                }
+            }
+        }
+        return missing;
+    }
+
+    private boolean isPositive(final int number) {
+        return number > 0.0;
+    }
 }
