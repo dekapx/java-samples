a0d186bb33c364a6eb29ebebe0268f392b6954de
KAPIL KUMAR <dekapx@KAPILs-MacBook-Pro-2.local>
2020-04-30 13:46:22 +0100

    test

--- java-core/src/main/java/com/dekapx/java/basics/Test.java
+++ java-core/src/main/java/com/dekapx/java/basics/Test.java
@@ -1,12 +1,33 @@
 package com.dekapx.java.basics;
 
-import com.google.common.collect.Lists;
+import lombok.SneakyThrows;
 
-import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
 public class Test {
     public static void main(String[] args) {
-        final List<Integer> list = Lists.newArrayList(1, 2, 3, 4, 5);
-        System.out.println(list.remove(1));
+        Runnable runnable = () -> {
+            IntStream.range(0, 5).forEach(i -> System.out.println(i));
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+        new Thread(runnable).start();
+    }
+
+    private static void runnableExampleOne() {
+        Runnable runnable = new Runnable() {
+            @SneakyThrows
+            @Override
+            public void run() {
+                IntStream.range(0, 5).forEach(i -> System.out.println(i));
+                TimeUnit.SECONDS.sleep(1);
+            }
+        };
+
+        new Thread(runnable).start();
     }
 }
