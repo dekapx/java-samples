43ed308d35a70853adaa7d5668ea51d5525e38db
KAPIL <dekapx@MacBook-Pro.local>
2020-09-11 16:32:32 +0100

    lambda demo

--- java-functional/src/main/java/com/dekapx/java/basics/LambdaDemo.java
+++ java-functional/src/main/java/com/dekapx/java/basics/LambdaDemo.java
@@ -2,6 +2,8 @@ package com.dekapx.java.basics;
 
 public class LambdaDemo {
     public static void main(String[] args) {
-
+        final Runnable runnable = ()
+                -> System.out.println(Thread.currentThread().getName());
+        new Thread(runnable).start();
     }
 }
