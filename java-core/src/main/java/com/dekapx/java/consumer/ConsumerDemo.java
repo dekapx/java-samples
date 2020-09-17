8c4c386fe39992f9495d1cb4f1cfcfa817da2779
KAPIL <dekapx@MacBook-Pro.local>
2020-09-17 16:01:55 +0100

    enum example

--- java-core/src/main/java/com/dekapx/java/consumer/ConsumerDemo.java
+++ java-core/src/main/java/com/dekapx/java/consumer/ConsumerDemo.java
@@ -2,15 +2,23 @@ package com.dekapx.java.consumer;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Consumer;
 
 public class ConsumerDemo {
     public static void main(String[] args) {
         final List<String> languages = Arrays.asList("Java", "Scala", "Kotlin");
         languages.forEach(print);
+
+        final Optional<String> optional = Optional.ofNullable("Test");
+        optional.ifPresent(printConsumer);
     }
 
     private static Consumer<String> print = (arg) -> {
         System.out.println(arg);
+    };
+
+    private static Consumer<String> printConsumer = (str) -> {
+        System.out.println(str);
     };
 }
