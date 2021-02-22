9d5eb33af6dec146d16333de989128c301fe99cf
KAPIL <dekapx@MacBook-Pro.local>
2021-02-22 10:08:44 +0000

    optional with list

--- java-functional/src/main/java/com/dekapx/java/cleancode/Example3.java
+++ java-functional/src/main/java/com/dekapx/java/cleancode/Example3.java
@@ -1,5 +1,7 @@
 package com.dekapx.java.cleancode;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -7,6 +9,8 @@ public class Example3 {
     public static void main(String[] args) {
         checkNullAndPrint("some value");
         checkNullAndPrintOptional("dummy value");
+
+        checkNullOnCollection();
     }
 
     private static void checkNullAndPrint(String arg) {
@@ -23,5 +27,17 @@ public class Example3 {
         Optional.ofNullable(arg)
                 .filter(a -> a.length() > 0)    // this expression can be a predicate
                 .ifPresent(System.out::println);
+    }
+
+    private static void checkNullOnCollection() {
+        final List<String> list1 = null;
+        Optional.ofNullable(list1)
+                .filter(l -> ! l.isEmpty())
+                .ifPresent(l -> System.out.println(l.get(0)));
+
+        final List<String> list2 = Arrays.asList("Test");
+        Optional.ofNullable(list2)
+                .filter(l -> ! l.isEmpty())
+                .ifPresent(l -> System.out.println(l.get(0)));
     }
 }
