6bacfeb0c153a83eab8fcc9816678a7ea81ed48e
dekapx <xkapil@gmail.com>
2022-10-11 19:46:39 +0100

    code clean up

---
+++ java-functional-v1/src/main/java/com/dekapx/java/stream/StreamPartitioningByDemo.java
@@ -0,0 +1,16 @@
+package com.dekapx.java.stream;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class StreamPartitioningByDemo {
+    public static void main(String[] args) {
+        Map<Boolean, List<Integer>> map = IntStream.rangeClosed(1, 10)
+                .mapToObj(Integer::new)
+                .collect(Collectors.partitioningBy(i -> i > 5));
+
+        map.forEach();
+    }
+}
