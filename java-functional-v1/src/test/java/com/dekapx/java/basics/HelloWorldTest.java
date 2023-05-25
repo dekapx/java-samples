abe86834c4e727a1e8a53f6d9e3ebb1af0bca205
dekapx <xkapil@gmail.com>
2023-05-25 16:42:59 +0100

    concurrency demo

--- java-functional-v1/src/test/java/com/dekapx/java/basics/HelloWorldTest.java
+++ java-functional-v1/src/test/java/com/dekapx/java/basics/HelloWorldTest.java
@@ -1,7 +1,10 @@
 package com.dekapx.java.basics;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -9,11 +12,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @ExtendWith(MockitoExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class HelloWorldTest {
     @InjectMocks
     private HelloWorld helloWorld;
 
-    @Test
+    @Test @Order(1)
     public void sayHello_withCorrectInput_shouldReturnCorrectValue() {
         final String result = this.helloWorld.sayHello("Test");
         assertThat(result).isNotNull()
@@ -22,7 +26,7 @@ public class HelloWorldTest {
                 });
     }
 
-    @Test
+    @Test @Order(2)
     public void sayHello_shouldThrowException_whenNullOrEmptyInput() {
         Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
             this.helloWorld.sayHello(null);
