9c4bf0f3ccb2d8a444aa6666c7edc91ae7b8ded3
dekapx <xkapil@gmail.com>
2023-05-29 22:59:18 +0100

    java functional quickstart

--- java-functional-v2/src/main/java/com/dekapx/java/predicate/BiPredicateDemo.java
+++ java-functional-v2/src/main/java/com/dekapx/java/predicate/BiPredicateDemo.java
@@ -1,9 +1,11 @@
 package com.dekapx.java.predicate;
 
+import com.dekapx.java.model.BodyType;
 import com.dekapx.java.model.Car;
+import com.dekapx.java.model.FuelType;
 
 import java.util.List;
-import java.util.function.Predicate;
+import java.util.function.BiPredicate;
 import java.util.stream.Collectors;
 
 import static com.dekapx.java.model.BodyType.SALOON;
@@ -12,17 +14,18 @@ import static com.dekapx.java.model.FuelType.DIESEL;
 import static com.dekapx.java.model.FuelType.ELECTRIC;
 import static com.dekapx.java.model.FuelType.HYBRID;
 
-public class PredicateDemo {
-    private static Predicate<Car> bodyTypeSaloonPredicate = (car) ->
-            car.bodyType().equals(SALOON);
-    private static Predicate<Car> fuelTypeElectricPredicate = (car) ->
-            car.fuelType().equals(ELECTRIC);
+public class BiPredicateDemo {
+    private static BiPredicate<Car, BodyType> bodyTypePredicate = (car, bodyType) ->
+            car.bodyType().equals(bodyType);
+    private static BiPredicate<Car, FuelType> fuelTypePredicate = (car, fuelType) ->
+            car.fuelType().equals(fuelType);
 
     public static void main(String[] args) {
         List<Car> inventory = getInventory();
         List<Car> electricSaloons = inventory
                 .stream()
-                .filter(bodyTypeSaloonPredicate.and(fuelTypeElectricPredicate))
+                .filter(car -> bodyTypePredicate.test(car, SALOON)
+                        && fuelTypePredicate.test(car, ELECTRIC))
                 .collect(Collectors.toList());
 
         electricSaloons.forEach(System.out::println);
