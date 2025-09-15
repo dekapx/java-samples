9b008fca2e8ee143a7e5f8bfe1bc12734182ef47
KAPIL KUMAR <dekapx@KAPILs-MacBook-Pro-M2.local>
2025-09-15 23:10:21 +0100

    code samples

--- java-functional-v2/src/main/java/com/dekapx/java/basics/Test.java
+++ java-functional-v2/src/main/java/com/dekapx/java/basics/Test.java
@@ -1,7 +1,22 @@
 package com.dekapx.java.basics;
 
+
+interface HelloWorld {
+    String sayHello(String arg);
+
+    default String sayGoodbye(String arg) {
+        return "Goodbye " + arg;
+    }
+
+    static String sayWelcome(String arg) {
+        return "Welcome " + arg;
+    }
+}
 public class Test {
     public static void main(String[] args) {
-
+        HelloWorld helloWorld = (arg) -> "Hello " + arg;
+        System.out.println(helloWorld.sayHello("World"));
+        System.out.println(helloWorld.sayGoodbye("World"));
+        System.out.println(HelloWorld.sayWelcome("World"));
     }
 }
