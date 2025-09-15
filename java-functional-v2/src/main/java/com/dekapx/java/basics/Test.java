cac6328ac18c019efdc275d0e67917ab4bc0d2b6
KAPIL KUMAR <dekapx@KAPILs-MacBook-Pro-M2.local>
2025-09-15 20:15:19 +0100

    code samples

--- java-functional-v2/src/main/java/com/dekapx/java/basics/Test.java
+++ java-functional-v2/src/main/java/com/dekapx/java/basics/Test.java
@@ -1,6 +1,10 @@
 package com.dekapx.java.basics;
 
+import java.util.List;
+
 public class Test {
     public static void main(String[] args) {
+        List<String> list = List.of("Java", "Python", "JavaScript", "C#", "C++");
+        list.forEach(System.out::println);
     }
 }
