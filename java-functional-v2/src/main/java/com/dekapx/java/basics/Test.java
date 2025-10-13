de8ea5e1ab7f9a9e7cb91c77712838226ea3874d
KAPIL KUMAR <dekapx@KAPILs-MacBook-Pro-M2.local>
2025-10-13 18:32:48 +0100

    code refactoring

--- java-functional-v2/src/main/java/com/dekapx/java/basics/Test.java
+++ java-functional-v2/src/main/java/com/dekapx/java/basics/Test.java
@@ -1,22 +1,8 @@
 package com.dekapx.java.basics;
 
-
-interface HelloWorld {
-    String sayHello(String arg);
-
-    default String sayGoodbye(String arg) {
-        return "Goodbye " + arg;
-    }
-
-    static String sayWelcome(String arg) {
-        return "Welcome " + arg;
-    }
-}
 public class Test {
-    public static void main(String[] args) {
-        HelloWorld helloWorld = (arg) -> "Hello " + arg;
-        System.out.println(helloWorld.sayHello("World"));
-        System.out.println(helloWorld.sayGoodbye("World"));
-        System.out.println(HelloWorld.sayWelcome("World"));
+    static void main(String[] args) {
     }
+
+
 }
