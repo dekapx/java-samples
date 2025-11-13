42017380170ad4f7efe8555f1f00084658f2f499
KAPIL KUMAR <dekapx@KAPILs-MacBook-Pro-M2.local>
2025-11-13 20:40:40 +0000

    code refactoring

--- java-functional-v2/src/main/java/com/dekapx/java/basics/Test.java
+++ java-functional-v2/src/main/java/com/dekapx/java/basics/Test.java
@@ -1,6 +1,19 @@
 package com.dekapx.java.basics;
 
+import com.dekapx.java.basics.lambdas.HelloWorld;
+
 public class Test {
     static void main(String[] args) {
+        HelloWorld helloWorld = helloWorldBuilder();
+        helloWorldExecutor(helloWorld, "World");
+    }
+
+    private static HelloWorld helloWorldBuilder() {
+        return name -> "Hello, " + name + "!";
+    }
+
+    private static void helloWorldExecutor(HelloWorld helloWorld, String name) {
+        String message = helloWorld.sayHello(name);
+        System.out.println(message);
     }
 }
---
+++ java-functional-v2/src/main/java/com/dekapx/java/basics/lambdas/HelloWorld.java
@@ -0,0 +1,6 @@
+package com.dekapx.java.basics.lambdas;
+
+@FunctionalInterface
+public interface HelloWorld {
+    String sayHello(String name);
+}
